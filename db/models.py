ff758f8feab0d6b9d9c7997d6b5ccbcd88898817
[2기-1반] 강석영 <ksge1124@gmail.com>
2022-12-26 23:59:11 +0900

    migrate

--- db/models.py
+++ db/models.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 import datetime
 from sqlalchemy.schema import FetchedValue
 import enum
-
+from sqlalchemy.orm import column_property, ColumnProperty
+from sqlalchemy.sql import expression as sa_exp
+from sqlalchemy.sql import func as sa_func
 
 # orm 매핑 함수 선언
 Base = declarative_base()
@@ -20,8 +22,6 @@ class UserRoleEnum(int, enum.Enum):
     Owner = 50
     Admin = 25
     User = 0
-
-
 
 class Board(Base):
     __tablename__ = "board"
@@ -48,6 +48,27 @@ class Board(Base):
         return result
 
 
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(150), nullable=False)
+    content = Column(Text, nullable=True, default=None)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=sql_text("CURRENT_TIMESTAMP"),
+        nullable=False, 
+    )
+    updated_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=sql_text("CURRENT_TIMESTAMP"),
+        server_onupdate=FetchedValue()
+    )
+    like_cnt: ColumnProperty
+    written_user_id = Column(Integer, ForeignKey("user.id"), index=True)
+    board_id = Column(Integer, ForeignKey("board.id"), index=True)
+
 class User(Base):
     __tablename__ = "user"
 
@@ -69,48 +90,69 @@ class User(Base):
     
 
 
+class Like(Base):
+    __tablename__ = "like"
 
-# class Post(Base):
-#     __tablename__ = "post"
-#     title = Column(String(150), nullable=False)
-#     content = Column(Text, nullable=True, default=None)
+    post_id = Column(Integer, ForeignKey("post.id"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=True)
+    user_id = Column(Integer, ForeignKey("user.id"), index=True)
+    post_id = Column(Integer, ForeignKey("post.id"), index=True)
+    parent_comment_id = Column(Integer, ForeignKey("comment.id"), index=True)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=sql_text("CURRENT_TIMESTAMP"),
+        nullable=False, 
+    )
+    updated_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=sql_text("CURRENT_TIMESTAMP"),
+        server_onupdate=FetchedValue()
+    )
+
+
+# class Hashtag(Base):
+#     __tablename__ = "hashtag"
+
+#     id = Column(Integer, primary_key=True)
+#     hashtag = Column(String, nullable=False)
 #     created_at = Column(
-#         DateTime, nullable=False, default=lambda: datetime.datetime.now(TZ_UTC)
+#         TIMESTAMP(timezone=True),
+#         server_default=sql_text("CURRENT_TIMESTAMP"),
+#         nullable=False, 
 #     )
 #     updated_at = Column(
-#         DateTime,
+#         TIMESTAMP(timezone=True),
 #         nullable=False,
-#         default=lambda: datetime.datetime.now(TZ_UTC),
-#         onupdate=lambda: datetime.datetime.now(TZ_UTC),
+#         server_default=sql_text("CURRENT_TIMESTAMP"),
+#         server_onupdate=FetchedValue()
 #     )
 
-#     written_user_id = Column(Integer, ForeignKey("user.id"), index=True)
-#     board_id = Column(Integer, ForeignKey("board.id"), index=True)
 
-
-# class Like(Base):
-#     __tablename__ = "like"
-
+# class PostHashTag(Base):
+#     """connect Post & Tag"""
+#     __tablename__ = "hashtag"
+#     id = Column(Integer, primary_key=True)
 #     post_id = Column(Integer, ForeignKey("post.id"), index=True)
-#     user_id = Column(Integer, ForeignKey("user.id"), index=True)
+#     user_id = Column(Integer, ForeignKey("hashtag.id"), index=True)
 
 
-# class Comment(Base):
-#     __tablename__ = "comment"
+# class Attachment(Base):
+#     __tablename__ = "attachment"
 
-#     content = Column(Text, nullable=True)
-
-#     user_id = Column(Integer, ForeignKey("user.id"), index=True)
+#     path = Column(Text, primary_key=True)
 #     post_id = Column(Integer, ForeignKey("post.id"), index=True)
-#     parent_comment_id = Column(Integer, ForeignKey("post.id"), index=True)
-#     # TODO: 재귀 FK search
-#     # models.ForeignKey(
-#     #     "self",
-#     #     on_delete=models.CASCADE,
-#     #     blank=True,
-#     #     null=True,  # nullable
-#     # )
-#     created_at = Column(DateTime, nullable=False)
-#     updated_at = Column(DateTime, nullable=False)
 
-
+Post.like_cnt = column_property(
+    sa_exp.select(sa_func.count(Like.user_id))
+    .where(Like.post_id == Post.id)
+    .correlate_except(Like)
+    .scalar_subquery()
+)
