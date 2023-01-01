78bb079d9f765f9e32b841f05f2dc105729f1ec5
[2기-1반] 강석영 <ksge1124@gmail.com>
2023-01-01 21:37:43 +0900

    update router

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,11 +1,17 @@
+from .auth import router as auth_router
+from .user import router as user_router
 from .board import router as board_router
 from .post import router as post_router
 from .comment import router as comment_router
-from .auth import router as auth_router
+from .hashtag import router as hashtag_router
+from .attachment import router as attachment_router
 
 API_ROUTERS = [
-    board_router, 
-    auth_router, 
-    post_router, 
-    comment_router
+    auth_router,
+    user_router,
+    board_router,
+    post_router,
+    comment_router,
+    hashtag_router,
+    attachment_router
 ]
