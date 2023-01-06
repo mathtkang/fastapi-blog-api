0c6940bf72931294405e19c5f8a04db15d35659b
[2기-1반] 강석영 <ksge1124@gmail.com>
2023-01-06 16:47:40 +0900

    convert router about 'get' to 'search-filter'

--- apis/comment.py
+++ apis/comment.py
@@ -7,7 +7,8 @@ from sqlalchemy.sql import expression as sql_exp
 from pydantic import BaseModel, Field
 import datetime
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_403_FORBIDDEN
-
+from typing import Literal
+from sqlalchemy.sql import func as sql_func
 
 router = APIRouter(prefix="/posts/{post_id:int}/comments", tags=["comments"])
 
@@ -24,17 +25,96 @@ class GetCommentResponse(BaseModel):
     class Config:
         orm_mode = True
 
-# TODO: convert to get one comment
+
 @router.get("/")
-def read_comment(
+def get_all_comments(
     post_id: int,
     session: Session = Depends(get_session),
 ):
     comments: list[m.Comment] = (
         session.execute(sql_exp.select(m.Comment)).scalars().all()
     )
-
     return [GetCommentResponse.from_orm(comment) for comment in comments]
+
+
+@router.get("/{comment_id:int}")
+def get_comment(
+    comment_id: int,
+    session: Session = Depends(get_session),
+):
+    comment: m.Comment = session.execute(
+        sql_exp.select(m.Comment).where(m.Comment.id == comment_id)
+    ).scalar_one_or_none()
+
+    if comment is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail=f"Comment with id {comment_id} not found",
+        )
+
+    return GetCommentResponse.from_orm(comment)
+
+
+class SearchCommentRequest(BaseModel):
+    # filter (query parameters)
+    parent_comment_id: int | None
+    written_user_id: int | None
+    post_id: int | None
+    # sort
+    sort_by: Literal["created_at"] | Literal["updated_at"] | Literal[
+        "written_user_id"
+    ] = "created_at"
+    sort_direction: Literal["asc"] | Literal["desc"] = "asc"
+    # pagination
+    offset: int = 0
+    count: int = 5
+
+
+class SearchCommentResponse(BaseModel):
+    comments: list[SearchCommentRequest]
+    count: int
+
+
+# DONE: convert to get one comment
+@router.post("/search")
+def search_comments(
+    q: SearchCommentRequest,
+    session: Session = Depends(get_session),
+):
+    comment_query = sql_exp.select(m.Comment)
+
+    if q.parent_comment_id is not None:
+        comment_query = comment_query.where(
+            m.Comment.parent_comment_id == q.parent_comment_id
+        )
+    if q.user_id is not None:
+        comment_query = comment_query.where(m.Comment.user_id == q.written_user_id)
+    if q.post_id is not None:
+        comment_query = comment_query.where(m.Comment.post_id == q.post_id)
+
+    comment_cnt: int = session.scalar(
+        sql_exp.select(sql_func.count()).select_from(comment_query)
+    )
+
+    sort_by_column = {
+        "created_at": m.Comment.created_at,
+        "updated_at": m.Comment.updated_at,
+        "written_user_id": m.Comment.user_id,
+    }[q.sort_by]
+    sort_exp = {
+        "asc": sort_by_column.asc(),
+        "desc": sort_by_column.desc(),
+    }[q.sort_direction]
+
+    comment_query = comment_query.order_by(sort_exp)
+
+    comment_query = comment_query.offset(q.offset).limit(q.count)
+    comments = session.execute(comment_query).scalars().all()
+
+    return SearchCommentResponse(
+        comments=[SearchCommentRequest.from_orm(comment) for comment in comments],
+        count=comment_cnt
+    )
 
 
 class PostCommentRequest(BaseModel):
