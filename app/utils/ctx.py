334fbc045176ee1de5e9f01b6aa98aa0244ef8ac
[2기-1반] 강석영 <ksge1124@gmail.com>
2023-02-01 02:05:16 +0900

    change routers by applying a context variable

--- app/utils/ctx.py
+++ app/utils/ctx.py
@@ -1,19 +1,30 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING, NamedTuple
 from mypy_boto3_s3 import S3Client
-
+from contextvars import ContextVar
 
 if TYPE_CHECKING:
     from app.settings import AppSettings
-    from app.database.db import engine
+    from app.database.db import DbConn
+
+
+
+
+_current_app_ctx: ContextVar[AppCtx] = ContextVar("_current_app_ctx")
+
+class _current_app_ctx_getter:
+    def __get__(self, obj, objtype=None):  # type: ignore
+        return _current_app_ctx.get()
 
 
 class AppCtx(NamedTuple):
-    # if TYPE_CHECKING:  # type: ignore
-    #     current: AppCtx  # question
+    if TYPE_CHECKING:  # type: ignore
+        current: AppCtx
 
-    app_settings: AppSettings
-    # settings: AppSettings
-    db: engine
+    settings: AppSettings
+    db: DbConn
     s3: S3Client
 
     id: str | None = None
+
+AppCtx.current = _current_app_ctx_getter()  # type: ignore
