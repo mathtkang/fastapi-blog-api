80ffbbce2207bdd4925c3dbf420d10d9cf3e79bb
sgaeng <ksge1124@gmail.com>
2023-02-09 20:58:58 +0900

    update test-code for board

--- test/unit/apis/test_board.py
+++ test/unit/apis/test_board.py
@@ -4,10 +4,11 @@ from httpx import AsyncClient
 from app.settings import AppSettings
 from test.helper import with_app_ctx, ensure_fresh_env
 from test.mock.user import create_user
-from test.utils import get_board
+from test.utils import search_board
 
 
-BOARD_TITLE = "This is created board title for testing"
+BOARD_TITLE="This is a board title for the test"
+UPDATED_BOARD_TITLE="This is a updated board title for the testf"
 
 class TestBoard:
     # 테스트 함수 실행시, 필요한 미리 정의된 입력값
@@ -21,6 +22,7 @@ class TestBoard:
             await ensure_fresh_env()
             await create_user(app_client)
 
+
     @pytest.mark.asyncio
     async def test_create_board(app_client: AsyncClient, user_access_token: str):
         response = await app_client.post(
@@ -29,23 +31,66 @@ class TestBoard:
                 "title": BOARD_TITLE,
             },
             headers={
-                "Authorization": "Bearer " + user_access_token
+                "Authorization": f"Bearer {user_access_token}"
             }
         )
+
         assert response.status_code == 200
         assert response.json() == {
-            "message": "success created board"
+            "message": "Success created board"
         }
+
 
     @pytest.mark.asyncio
     async def test_get_board(app_client: AsyncClient):
         board_id = (
-            await get_board(app_client, BOARD_TITLE)
+            await search_board(app_client, BOARD_TITLE)
         )['id']
 
         response = await app_client.get(
             f"/boards/{board_id}",
         )
+
         assert response.status_code == 200
         assert response.json()['id'] == board_id
         assert response.json()['title'] == BOARD_TITLE
+
+
+    # DONE: 여기부터
+    @pytest.mark.asyncio
+    async def test_update_board(app_client: AsyncClient, user_access_token: str):
+        board_id = (
+            await search_board(app_client, BOARD_TITLE)
+        )['id']
+
+        response = await app_client.put(
+            f"/boards/{board_id}",
+            json={
+                "title": UPDATED_BOARD_TITLE,
+            },
+            headers={
+                "Authorization": f"Bearer {user_access_token}"
+            }
+        )
+        assert response.status_code == 200
+        assert response.json() == {
+            "message": "Success updated board"
+        }
+
+
+    @pytest.mark.asyncio
+    async def test_delete_board(app_client: AsyncClient, user_access_token: str):
+        board_id = (
+            await search_board(app_client, UPDATED_BOARD_TITLE)  # Question: UPDATED_BOARD_TITLE 으로 하는게 더 좋을까요?
+        )['id']
+
+        response = await app_client.delete(
+            f"/boards/{board_id}",
+            headers={
+                "Authorization": f"Bearer {user_access_token}"
+            }
+        )
+        assert response.status_code == 200
+        assert response.json() == {
+            "message": "Success deleted board"
+        }
