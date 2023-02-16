7902eafe2299446b929372feffb790587731127a
sgaeng <ksge1124@gmail.com>
2023-02-16 22:40:45 +0900

    create hashtag router for search

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -2,6 +2,7 @@ from .attachment import router as attachment_router
 from .auth import router as auth_router
 from .board import router as board_router
 from .comment import router as comment_router
+from .hashtag import router as hashtag_router
 from .post import router as post_router
 from .user import router as user_router
 
@@ -12,6 +13,7 @@ API_ROUTERS = [
     auth_router,
     board_router,
     comment_router,
+    hashtag_router,
     post_router,
     user_router,
 ]
