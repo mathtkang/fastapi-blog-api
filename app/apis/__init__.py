589bb65cb982da3be1e3e947620ad8cb96ebd9dd
sgaeng <ksge1124@gmail.com>
2023-02-21 23:40:14 +0900

    remove attachment

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,4 +1,3 @@
-from .attachment import router as attachment_router
 from .auth import router as auth_router
 from .board import router as board_router
 from .comment import router as comment_router
@@ -9,7 +8,6 @@ from .user import router as user_router
 __all__ = ["ALL_ROUTERS"]
 
 API_ROUTERS = [
-    attachment_router,
     auth_router,
     board_router,
     comment_router,
--- app/apis/attachment.py
+++ app/apis/attachment.py
@@ -1,65 +1,32 @@
-from fastapi import Depends, APIRouter, HTTPException, UploadFile
-from app.utils.auth import resolve_access_token
-from sqlalchemy.ext.asyncio import AsyncSession as Session
-from pydantic import BaseModel
-from app.database import models as m
-from sqlalchemy.sql import expression as sql_exp
-from app.utils.blob import upload_image
-from starlette.status import HTTP_403_FORBIDDEN
-from mypy_boto3_s3.client import S3Client
-from app.utils.ctx import AppCtx
+# from fastapi import Depends, APIRouter, HTTPException, UploadFile
+# from pydantic import BaseModel
+# from sqlalchemy.ext.asyncio import AsyncSession as Session
+# from sqlalchemy.sql import expression as sql_exp
+# from starlette.status import HTTP_404_NOT_FOUND
+# from mypy_boto3_s3.client import S3Client
+
+# from app.database import models as m
+# from app.utils.auth import resolve_access_token, validate_user_role
+# from app.utils.blob import upload_image
+# from app.utils.ctx import AppCtx
 
 
-router = APIRouter(prefix="/attachment", tags=["attachment"])
+# router = APIRouter(prefix="/attachment", tags=["attachment"])
 
 
-class PostAttachmentResponse(BaseModel):
-    bucket: str
-    key: str
 
 
-@router.post("")
-async def post_attachment(
-    file: UploadFile,
-    user_id: int = Depends(resolve_access_token),
-):
-    """
-    파일스토리지
-    1. session
-    2. resolve access token (user_id)
-    3. blob client
-    4. 사용자의 입력(file)
-    """
-    user: m.User | None = await AppCtx.current.db.session.scalar(
-        sql_exp.select(m.User).where(m.User.id == user_id)
-    )
-
-    if user is None:
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN,
-            detail="User not found",
-        )
-
-    attachment_file_key = await upload_image(
-        user_id, 
-        file.filee, 
-        file.file
-    )
-
-    return PostAttachmentResponse(bucket="fastapi-practice", key=attachment_file_key)
-
-
-# 유저 profile_file 가져오기
+# # 프로필 '파일만' 삭제
+# # @router.delete("")
+# # async def delete_attachment():
 
 
 
 
 
+# # URL: 제한시간, 해당 리소스에만 접근 가능
 
-
-# URL: 제한시간, 해당 리소스에만 접근 가능
-
-# 1. upload file by this endpoint
-# 2. get the attachment file key
-# 3. upload attachment key to post or user
-# 4. get attachment file url by user get endpoint or post get endpoint
+# # 1. upload file by this endpoint
+# # 2. get the attachment file key
+# # 3. upload attachment key to post or user
+# # 4. get attachment file url by user get endpoint or post get endpoint
