298c5cce98c564a2a67511542555a67fc8762360
sgaeng <ksge1124@gmail.com>
2023-02-22 23:59:14 +0900

    create owner access token

--- test/constants.py
+++ test/constants.py
@@ -1,2 +1,4 @@
 DEFAULT_USER_EMAIL = "user@example.com"
 DEFAULT_USER_PASSWORD = "default_user_password"
+DEFAULT_OWNER_EMAIL = "owner@example.com"
+DEFAULT_OWNER_PASSWORD = "default_owner_password"
