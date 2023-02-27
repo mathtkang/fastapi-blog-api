012a34be0f67744eb719f3026f26e8fc350b4f1f
sgaeng <ksge1124@gmail.com>
2023-02-27 23:58:25 +0900

    create_owner_user at mock

--- test/mock/user.py
+++ test/mock/user.py
@@ -1,5 +1,13 @@
 from httpx import AsyncClient
-from test.constants import DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD
+from test.constants import (
+    DEFAULT_USER_EMAIL, 
+    DEFAULT_USER_PASSWORD, 
+    DEFAULT_OWNER_EMAIL,
+    DEFAULT_OWNER_PASSWORD,
+)
+from app.utils.ctx import AppCtx
+from sqlalchemy.sql import expression as sql_exp
+from app.database import models as m
 
 
 async def create_user(app_client: AsyncClient) -> None:
@@ -10,5 +18,24 @@ async def create_user(app_client: AsyncClient) -> None:
             "password": DEFAULT_USER_PASSWORD,
         },
     )
+    return response
 
-    assert response.status_code == 200
+
+async def create_owner(app_client: AsyncClient) -> None:
+    response = await app_client.post(
+        '/auth/signup',
+        json={
+            "email": DEFAULT_OWNER_EMAIL,
+            "password": DEFAULT_OWNER_PASSWORD,
+        },
+    )
+
+    user = await AppCtx.current.db.session.scalar(
+        sql_exp.select(m.User).where(m.User.email == DEFAULT_OWNER_EMAIL)
+    )
+    user.role = m.UserRoleEnum.Owner
+
+    AppCtx.current.db.session.add(user)
+    await AppCtx.current.db.session.commit()
+
+    return response
