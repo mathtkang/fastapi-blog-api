37134c7a792c1cd65c85404644bb25fc3ab2bf6d
sgaeng <ksge1124@gmail.com>
2023-02-28 23:51:10 +0900

    add constants

--- test/constants.py
+++ test/constants.py
@@ -1,4 +1,18 @@
+# default user
 DEFAULT_USER_EMAIL = "user@example.com"
 DEFAULT_USER_PASSWORD = "default_user_password"
+# default owner
 DEFAULT_OWNER_EMAIL = "owner@example.com"
 DEFAULT_OWNER_PASSWORD = "default_owner_password"
+# board title
+BOARD_TITLE="This is a Board Title for the test."
+UPDATED_BOARD_TITLE="This is a Updated Board Title for the test."
+# post title
+POST_TITLE="This is a Post Title for the test."
+UPDATED_POST_TITLE="This is a Updated Post Title for the test."
+# post content
+POST_CONTENT="This is a Post Content for the test. #hashtag"
+UPDATED_POST_CONTENT="This is a Updated Post Content for the test. #add_hashtag"
+# comment content
+COMMENT_CONTENT="This is a Comment Content for the test."
+UPDATED_COMMENT_CONTENT="This is a Updated Comment Content for the test."
