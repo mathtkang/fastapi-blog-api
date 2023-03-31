e2e1d99d31222200871a7ad7422bfbcd5789338e
sgaeng <ksge1124@gmail.com>
2023-04-01 01:15:42 +0900

    .

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app_settings = AppSettings()
 app = create_app(app_settings)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5468)
+    uvicorn.run(app, host="127.0.0.1", port=5468)
